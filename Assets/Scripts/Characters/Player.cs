a6833e7213647f4ec11b289207029a9d5dc235a4
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2019-08-29 09:52:23 +1000

    Basic platformer stuff

---
+++ Assets/Scripts/Characters/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController2D controller;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+     }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        bool isJumping = Input.GetButtonDown("Jump");
+
+        if (isJumping)
+        {
+
+        }
+
+        controller.Move(horizontal * moveSpeed);
+    }
+}
