9dec0427e9d38b33364f7acafdcc13a1aa89100a
GlitcherOG <lachlan.kalf@tafensw.net.au>
2019-09-02 11:31:52 +1000

    Stuff

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController2D controller;
-    public float moveSpeed;
+
+    public float moveSpeed = 10f;
+    public float jumpHeight = 5f;
+    public float climbSpeed = 10f;
+    public float portalDistance = 1f;
     // Start is called before the first frame update
+
+    private GameObject currentPortal;
     void Start()
     {
         controller = GetComponent<CharacterController2D>();
-     }
+    }
 
 
     // Update is called once per frame
@@ -22,9 +28,47 @@ public class Player : MonoBehaviour
 
         if (isJumping)
         {
-            controller.Jump(5);
+            controller.Jump(jumpHeight);
         }
-
+        controller.Climb(vertical * climbSpeed);
         controller.Move(horizontal * moveSpeed);
+
+        if (currentPortal != null)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                currentPortal.SendMessage("Interact");
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(currentPortal != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(currentPortal.transform.position, portalDistance);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Item"))
+        {
+            Destroy(col.gameObject);
+            GameManager.Instance.AddScore(1);
+        }
+        if (col.CompareTag("Portal"))
+        {
+            currentPortal = col.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Portal"))
+        {
+            currentPortal = null;
+        }
     }
 }
---
+++ Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UnityEvent onEnter, OnStay, onExit;
+    public UnityEvent onInteract;
+    public string hitTag = "Player";
+
+    public void Interact()
+    {
+        onInteract.Invoke();
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag(hitTag))
+        {
+            onEnter.Invoke();
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        onEnter.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onExit.Invoke();
+    }
+}
