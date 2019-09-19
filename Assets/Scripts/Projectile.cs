795b0ca472563dc1311080d280ddb7fac194f2dd
Undeddy-Gamer <ed.dixen@gmail.com>
2019-09-19 15:46:57 +1000

    base shoot script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject projectile;
+    public float projectileSpeed = 5;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(projectileSpeed, 0));
+                
+                //AddForce(transform.forward * 10);
+        }
+    }
+
+}
