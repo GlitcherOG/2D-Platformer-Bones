fc1deb8f50b3b3f9d1da55e69c08535126a608a5
Undeddy-Gamer <ed.dixen@gmail.com>
2019-09-26 10:54:37 +1000

    Projectile Mk2

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public GameObject projectile;
+    public GameObject projectilePrefab;
+
     public float projectileSpeed = 5;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
+
+            Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (Vector2)((worldMousePos - transform.position));
+            //direction.Normalize();
+            //Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
             GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-            bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(projectileSpeed, 0));
-                
-                //AddForce(transform.forward * 10);
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+            //transform.TransformDirection(new Vector2(projectileSpeed, 0));
+            //direction * projectileSpeed;
+
+
+            StartCoroutine(RemoveProjectile(bullet, 5f));
+            //AddForce(transform.forward * 10);
         }
     }
+
+   
+    IEnumerator RemoveProjectile(GameObject bullet, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        Destroy(bullet);
+        // Now do your thing here
+    }
+
+
 
 }
---
+++ Assets/Scripts/ProjectileHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class ProjectileHandler : MonoBehaviour
+{
+
+    
+
+    void Update()
+    {
+        Vector2 moveDirection = gameObject.GetComponent<Rigidbody2D>().velocity;
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }      
+
+    }
+
+    
+}
