4ecb654f9b4d8d9aaf259ffceb8f57831e2ac9c3
GlitcherOG <lachlan.kalf@tafensw.net.au>
2019-10-21 14:48:05 +1100

    Added dummy scene and other textures

---
+++ Assets/Scripts/Camerafix.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Camerafix : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+    }
+}
