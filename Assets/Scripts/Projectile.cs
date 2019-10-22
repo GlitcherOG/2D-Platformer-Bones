7ddcacac5a2769fbbc35a0b14e8839ac793c0630
Eddie <ed.dixen@gmail.com>
2019-10-23 01:55:27 +1100

    Update Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,6 +7,8 @@ public class Projectile : MonoBehaviour
     public GameObject projectile;
 
     public float projectileSpeed = 5;
+    public float projectileDestroyTime = 5f;
+
     void Update()
     {
         Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane *10));
@@ -15,7 +17,9 @@ public class Projectile : MonoBehaviour
         {
             GameObject bullet = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
             bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector3(0, 0, projectileSpeed * 5));
-            StartCoroutine(RemoveProjectile(bullet, 5f));
+
+            //Destroy projectile after x seconds
+            StartCoroutine(RemoveProjectile(bullet, projectileDestroyTime));
         }
     }
 
@@ -24,7 +28,6 @@ public class Projectile : MonoBehaviour
     {
         yield return new WaitForSeconds(delayTime);
         Destroy(bullet);
-        // Now do your thing here
     }
 
 
