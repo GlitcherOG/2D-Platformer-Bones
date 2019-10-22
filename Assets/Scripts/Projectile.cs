f32280cabd2664effedc2cf2dccba46346a6337e
Eddie <ed.dixen@gmail.com>
2019-10-23 01:55:46 +1100

    Update Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -11,14 +11,21 @@ public class Projectile : MonoBehaviour
 
     void Update()
     {
+        // get the mouse position in the game
         Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane *10));
+
+        // 
         transform.LookAt(worldMousePos);
+
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
+            // Create a new projectile instance
             GameObject bullet = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+
+            // Give the projectile 'a boost' toward the mouse position (fire it)
             bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector3(0, 0, projectileSpeed * 5));
 
-            //Destroy projectile after x seconds
+            // Destroy projectile after x seconds so they don't stack up and take up resources
             StartCoroutine(RemoveProjectile(bullet, projectileDestroyTime));
         }
     }
@@ -26,7 +33,10 @@ public class Projectile : MonoBehaviour
    
     IEnumerator RemoveProjectile(GameObject bullet, float delayTime)
     {
+        // Start a delay on projectile instance
         yield return new WaitForSeconds(delayTime);
+
+        // Remove projectile
         Destroy(bullet);
     }
 
