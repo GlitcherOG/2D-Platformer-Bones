298f5d3ba860755ffdc1fd8e16b4d0b4db15e4c7
Eddie <ed.dixen@gmail.com>
2019-11-05 16:00:28 +1100

    base level 3

---
+++ Assets/Scripts/LimitCameraHoriz.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitCameraHoriz : MonoBehaviour
+{
+
+    
+    public float rightLimit;
+    public float leftLimit;
+    private float startY;
+
+    private void Start()
+    {
+        startY = transform.position.y;
+    }
+
+    void Update()
+    {
+        if (transform.position.x < leftLimit)
+        {
+            Debug.Log(transform.position.x + " : " + startY);
+            transform.position = transform.position + new Vector3(leftLimit, startY, 0);
+                //;
+        }
+
+
+    }
+}
