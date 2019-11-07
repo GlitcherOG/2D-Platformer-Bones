d5d4a2c051d8c3ace25996348d5aa35b51f6a5c9
Undeddy-Gamer <54134838+Undeddy-Gamer@users.noreply.github.com>
2019-11-07 11:59:00 +1100

    lev 3 update

---
+++ Assets/Scripts/Enemies/ShootLaser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootLaser : MonoBehaviour
+{
+    //Prefeab of laser asset
+    public Transform laserPrefab;
+    //position to instanciate laser
+    public Transform startPosition;
+
+    private void Start()
+    {
+        StartCoroutine(Zap());
+    }
+    
+
+    private IEnumerator Zap()
+    {
+        yield return new WaitForSeconds(5f);
+        //new GameObject("Laser").AddComponent<Fractal>().Initialize(this, i);
+        Transform laser = Instantiate(laserPrefab);
+        laser.position = startPosition.position;
+        StartCoroutine(Zap());
+    }
+}
